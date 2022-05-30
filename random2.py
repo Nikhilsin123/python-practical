c93e853fd61db260767b4dc955d026c9fc17d10d
Nikhilsin123 <106536676+Nikhilsin123@users.noreply.github.com>
2022-05-30 22:06:10 +0530

    Create random2.py

---
+++ random2.py
@@ -0,0 +1,7 @@
+import random
+lottery_ticket_list=[]
+print("creating 100 random lotter ticket")
+for i in range(100):
+  lottery_ticket_list.append(random.randrange(1000000000,9999999999)
+  winners=random.sample(lotter_ticket_list,2)
+  print(winners)
