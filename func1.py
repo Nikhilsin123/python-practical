ab1185460d30cbbbf9befe5b67909c7e43e7e69a
Nikhilsin123 <106536676+Nikhilsin123@users.noreply.github.com>
2022-05-30 19:56:35 +0530

    Create func1.py

---
+++ func1.py
@@ -0,0 +1,8 @@
+def detail(roll)
+x=[1,2,3,4]
+if roll in x:
+   print(f'roll number{roll} is present)
+else:
+    print(f'roll number{roll} is absent)
+roll=int(input("enter the roll number")
+detail(roll)
