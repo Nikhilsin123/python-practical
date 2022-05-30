656814eac500c90ba1879378c0a7f55a43c24d74
Nikhilsin123 <106536676+Nikhilsin123@users.noreply.github.com>
2022-05-30 20:03:16 +0530

    Create func2.py

---
+++ func2.py
@@ -0,0 +1,12 @@
+def count(val)
+vov=0
+con=0
+for i in range(len(val)):
+if val(i) in ['a','e','i','o','u']
+    vov=vov+1
+else:
+con=con+1
+print("count of vowel is",vov)
+print("count of consonant is",con)
+x=int(input("enter a number")
+count(x)
